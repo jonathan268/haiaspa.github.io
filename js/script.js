98bf307a20e8494389de60902886d1b34e1e2032
Jonathan Payan <jonathan.payan.pineda@gmail.com>
2022-02-08 00:00:41 -0500

    adjust to carousel animations

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+
+ var nav = document.querySelector('nav');
+
+ window.addEventListener('scroll', function () {
+     if (window.pageYOffset > 300) {
+         /*nav.classList.add('bg-dark', 'shadow');*/
+         nav.setAttribute("style", "background-color:RGB(242,242,242);");
+     } else {
+         nav.classList.remove('bg-dark', 'shadow');
+         nav.removeAttribute("style", "background-color:RGB(242,242,242);");
+
+
+     }
+
+ });
 /*let next = document.getElementsByClassName("carousel-control-next");
 let container= document.getElementsByClassName("carousel-caption");
 container.style.visibility="hidden";
