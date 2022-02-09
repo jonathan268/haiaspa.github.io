740d4bf30d14c68e2f67bed229a55e724c94bcae
Jonathan Payan <jonathan.payan.pineda@gmail.com>
2022-02-09 18:29:53 -0500

    first view

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-
+let cont=0;
  var nav = document.querySelector('nav');
 
  window.addEventListener('scroll', function () {
      if (window.pageYOffset > 300) {
          /*nav.classList.add('bg-dark', 'shadow');*/
          nav.setAttribute("style", "background-color:RGB(242,242,242);");
+         //nav.setAttribute("style", "background-color:;");
      } else {
          nav.classList.remove('bg-dark', 'shadow');
          nav.removeAttribute("style", "background-color:RGB(242,242,242);");
@@ -13,6 +14,23 @@
      }
 
  });
+
+
+function changeColorNav(){
+    let click=0;
+    nav.setAttribute("style", "background-color:RGB(242,242,242);");
+    let str = '';
+
+        cont=cont+1;
+        console.log(click+1);
+      
+}
+
+
+
+
+
+
 /*let next = document.getElementsByClassName("carousel-control-next");
 let container= document.getElementsByClassName("carousel-caption");
 container.style.visibility="hidden";
