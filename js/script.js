2a329a6ee84ee60582c0f579bfe68af998987693
Jonathan Payan <jonathan.payan.pineda@gmail.com>
2022-02-11 16:31:21 -0500

    new navbar

--- js/script.js
+++ js/script.js
@@ -4,11 +4,12 @@ let cont=0;
  window.addEventListener('scroll', function () {
      if (window.pageYOffset > 70) {
          /*nav.classList.add('bg-dark', 'shadow');*/
-         nav.setAttribute("style", "background-color:RGB(242,242,242);");
+         nav.setAttribute("style", "background-color:RGB(242,242,242) !important;");
          //nav.setAttribute("style", "background-color:;");
      } else {
-         nav.classList.remove('bg-dark', 'shadow');
-         nav.removeAttribute("style", "background-color:RGB(242,242,242);");
+         //nav.classList.remove('bg-dark', 'shadow');
+         nav.removeAttribute("style", "background-color:RGB(242,242,242) !important;");
+
 
 
      }
@@ -16,22 +17,8 @@ let cont=0;
  });
 
 
-function changeColorNav(){
-    let click=0;
-    nav.setAttribute("style", "background-color:RGB(242,242,242);");
-    let str = '';
-
-        cont=cont+1;
-        console.log(click+1);
-      
-}
 
 
 
 
 
-
-/*let next = document.getElementsByClassName("carousel-control-next");
-let container= document.getElementsByClassName("carousel-caption");
-container.style.visibility="hidden";
-*/
